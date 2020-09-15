5937bdfb65d8e6a3b1c9a2eadf2494481f765cc6
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-09-15 12:50:23 +0530

    Test

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen, VolunteerScreen, DonorScreen, LoadingScreen, BottomNav, Profile, Maps } from './src/screens'
 
 
--- src/screens/BottomNav/Maps.js
+++ src/screens/BottomNav/Maps.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
-import MapView from 'react-native-maps';
+import MapView from "react-native-maps";
 import { Text } from 'react-native-elements'
 const Maps = () => {
     return (
-        <MapView
-        style={{
-          flex: 1
-        }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
+        <View>
+            <Text>Hlo</Text>
+        </View>
     )
 }
 
