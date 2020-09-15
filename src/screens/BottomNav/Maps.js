dac29080e08c87a2865fbe847ac5ca834b6a5879
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-09-15 12:34:40 +0530

    Added Map Integration

--- src/screens/BottomNav/Maps.js
+++ src/screens/BottomNav/Maps.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { View } from 'react-native'
-import MapView from "react-native-maps";
+import MapView from 'react-native-maps';
 import { Text } from 'react-native-elements'
 const Maps = () => {
     return (
-        <View>
-            <Text>Hlo</Text>
-        </View>
+        <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
     )
 }
 
